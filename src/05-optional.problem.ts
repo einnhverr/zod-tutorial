7ecd9b778dad25a5b6e1bca5d3743346766ffca9
Erhard-Emil Muresan <mue2clj@bosch.com>
2023-07-26 10:24:27 +0000

    Add solution to exercise 05

--- src/05-optional.problem.ts
+++ src/05-optional.problem.ts
@@ -6,11 +6,14 @@ import { z } from "zod";
 const Form = z.object({
   name: z.string(),
   phoneNumber: z.string(),
-  //                     ^ 🕵️‍♂️
 });
 
+const partialForm = Form.partial({
+  phoneNumber: true,
+})
+
 export const validateFormInput = (values: unknown) => {
-  const parsedData = Form.parse(values);
+  const parsedData = partialForm.parse(values);
 
   return parsedData;
 };
